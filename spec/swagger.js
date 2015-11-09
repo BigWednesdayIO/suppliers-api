d2abedea913533bc8ec26b9808b9fba5eb589e83
Chris Impey <chris@bigwednesday.io>
2015-11-09 17:36:52 +0000

    create hapi server with swagger docs

---
+++ spec/swagger.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const swaggerSpec = require('swagger-tools').specs.v2;
+const expect = require('chai').expect;
+
+describe('/swagger', () => {
+  let swagger;
+
+  before(done => {
+    require('../lib/server')((err, server) => {
+      if (err) {
+        return console.error(err);
+      }
+
+      server.inject({url: '/swagger', method: 'GET'}, response => {
+        swagger = JSON.parse(response.payload);
+        done();
+      });
+    });
+  });
+
+  it('generates a valid swagger schema', () => {
+    swaggerSpec.validate(swagger, (err, result) => {
+      if (result && result.errors) {
+        console.error(result.errors);
+      }
+      if (result && result.warnings) {
+        console.error(result.warnings);
+      }
+
+      expect(err).to.not.be.ok;
+      expect(result).to.equal(undefined);
+    });
+  });
+});
