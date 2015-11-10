27ffdf9122ab0c6f8aaf752cd7696bcfe006c3cf
Chris Impey <chris@bigwednesday.io>
2015-11-10 16:20:53 +0000

    define PUT route

--- spec/suppliers.js
+++ spec/suppliers.js
@@ -25,8 +25,11 @@ describe('/suppliers', () => {
       expect(createResponse.headers.location).to.equal(`/suppliers/${createResponse.result.id}`);
     });
 
-    it('returns the supplier name', () => {
-      expect(createResponse.result.name).to.equal(createSupplierPayload.name);
+    it('returns the supplier resource', () => {
+      const resource = _.clone(createSupplierPayload);
+      resource.id = createResponse.result.id;
+
+      expect(createResponse.result).to.deep.equal(resource);
     });
 
     describe('validation', () => {
@@ -86,6 +89,58 @@ describe('/suppliers', () => {
       resource.id = createResponse.result.id;
 
       expect(getResponse.result).to.deep.equal(resource);
+    });
+  });
+
+  describe('put', () => {
+    let createResponse;
+    const createSupplierPayload = {id: 'SUP', name: 'A Supplier'};
+
+    before(() => {
+      return specRequest({url: '/suppliers/SUP', method: 'PUT', payload: createSupplierPayload})
+        .then(response => {
+          createResponse = response;
+        });
+    });
+
+    describe('as create', () => {
+      it('returns http 201', () => {
+        expect(createResponse.statusCode).to.equal(201);
+      });
+
+      it('returns created resource location', () => {
+        expect(createResponse.headers.location).to.equal(`/suppliers/${createResponse.result.id}`);
+      });
+
+      it('returns a supplier resource', () => {
+        const resource = _.clone(createSupplierPayload);
+        resource.id = createResponse.result.id;
+
+        expect(createResponse.result).to.deep.equal(resource);
+      });
+    });
+
+    describe('as update', () => {
+      let updateResponse;
+      const updatedSupplierPayload = {id: 'SUP', name: 'New name'};
+
+      before(() => {
+        return specRequest({url: '/suppliers/SUP', method: 'PUT', payload: updatedSupplierPayload})
+          .then(response => {
+            updateResponse = response;
+          });
+      });
+
+      it('returns http 200', () => {
+        expect(updateResponse.statusCode).to.equal(200);
+      });
+
+      it('returns a supplier resource', () => {
+        const resource = _.clone(updatedSupplierPayload);
+        resource.id = updateResponse.result.id;
+
+        expect(updateResponse.result).to.deep.equal(resource);
+      });
     });
   });
 });
