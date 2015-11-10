9ffd87f60572129ddc354686e4a08d20b4fe7300
Chris Impey <chris@bigwednesday.io>
2015-11-10 11:19:13 +0000

    store suppliers in memory and return from get route

--- spec/suppliers.js
+++ spec/suppliers.js
@@ -5,8 +5,9 @@ const expect = require('chai').expect;
 const specRequest = require('./spec_request');
 
 describe('/suppliers', () => {
+  const createSupplierPayload = {name: 'A Supplier'};
+
   describe('post', () => {
-    const createSupplierPayload = {name: 'A Supplier'};
     let createResponse;
 
     before(() => {
@@ -49,6 +50,33 @@ describe('/suppliers', () => {
             expect(response.result.message).to.equal('child "name" fails because ["name" must be a string]');
           });
       });
+    });
+  });
+
+  describe('get', () => {
+    let createResponse;
+    let getResponse;
+
+    before(() => {
+      return specRequest({url: '/suppliers', method: 'POST', payload: createSupplierPayload})
+        .then(response => {
+          createResponse = response;
+          return specRequest({url: response.headers.location, method: 'GET'});
+        })
+        .then(response => {
+          getResponse = response;
+        });
+    });
+
+    it('returns http 200', () => {
+      expect(getResponse.statusCode).to.equal(200);
+    });
+
+    it('returns the supplier resource', () => {
+      const resource = _.clone(createSupplierPayload);
+      resource.id = createResponse.result.id;
+
+      expect(getResponse.result).to.deep.equal(resource);
     });
   });
 });
