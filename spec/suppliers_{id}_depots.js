c9905814c3eef6aae4936d09bdf55587f33e57ee
Chris Impey <chris@bigwednesday.io>
2015-11-16 12:26:44 +0000

    validate depot payload

--- spec/suppliers_{id}_depots.js
+++ spec/suppliers_{id}_depots.js
@@ -18,7 +18,7 @@ describe('/suppliers/{id}/depots', () => {
 
   describe('post', () => {
     it('returns http 404 for a non existant supplier', () => {
-      return specRequest({url: '/suppliers/123/depots', method: 'POST', payload: {}})
+      return specRequest({url: '/suppliers/123/depots', method: 'POST', payload: {name: 'Depot 1'}})
         .then(response => {
           expect(response.statusCode).to.equal(404);
           expect(response.result).to.have.property('message', 'Supplier "123" not found.');
@@ -43,6 +43,40 @@ describe('/suppliers/{id}/depots', () => {
 
       const result = _.omit(createResponse.result, '_metadata');
       expect(result).to.deep.equal(resource);
+    });
+
+    describe('validation', () => {
+      it('requires name', () => {
+        const payload = _.omit(createPayload, 'name');
+
+        return specRequest({url: '/suppliers/1/depots', method: 'POST', payload})
+          .then(response => {
+            expect(response.statusCode).to.equal(400);
+            expect(response.result.message).to.equal('child "name" fails because ["name" is required]');
+          });
+      });
+
+      it('requires name to be a string', () => {
+        const payload = _.omit(createPayload, 'name');
+        payload.name = 123;
+
+        return specRequest({url: '/suppliers/1/depots', method: 'POST', payload})
+          .then(response => {
+            expect(response.statusCode).to.equal(400);
+            expect(response.result.message).to.equal('child "name" fails because ["name" must be a string]');
+          });
+      });
+
+      it('does not allow _metadata', () => {
+        const payload = _.clone(createPayload);
+        payload._metadata = {created: new Date()};
+
+        return specRequest({url: '/suppliers/1/depots', method: 'POST', payload})
+          .then(response => {
+            expect(response.statusCode).to.equal(400);
+            expect(response.result.message).to.equal('"_metadata" is not allowed');
+          });
+      });
     });
   });
 
