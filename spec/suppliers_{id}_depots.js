dd8315239ee26b3866ea0c5b1726f677e524baf4
Chris Impey <chris@bigwednesday.io>
2015-11-16 12:16:15 +0000

    support creation of simple depot resource

--- spec/suppliers_{id}_depots.js
+++ spec/suppliers_{id}_depots.js
@@ -1,9 +1,21 @@
 'use strict';
 
+const _ = require('lodash');
 const expect = require('chai').expect;
 const specRequest = require('./spec_request');
 
 describe('/suppliers/{id}/depots', () => {
+  const createPayload = {name: 'A depot'};
+  let createResponse;
+
+  before(() => {
+    return specRequest({url: '/suppliers/1', method: 'PUT', payload: {name: 'Supplier 1'}})
+      .then(() => specRequest({url: '/suppliers/1/depots', method: 'POST', payload: createPayload}))
+      .then(response => {
+        createResponse = response;
+      });
+  });
+
   describe('post', () => {
     it('returns http 404 for a non existant supplier', () => {
       return specRequest({url: '/suppliers/123/depots', method: 'POST', payload: {}})
@@ -11,6 +23,26 @@ describe('/suppliers/{id}/depots', () => {
           expect(response.statusCode).to.equal(404);
           expect(response.result).to.have.property('message', 'Supplier "123" not found.');
         });
+    });
+
+    it('returns http 201 for a new depot', () => {
+      expect(createResponse.statusCode).to.equal(201);
+    });
+
+    it('returns created resource location', () => {
+      expect(createResponse.headers.location).to.equal(`/suppliers/1/depots/${createResponse.result.id}`);
+    });
+
+    it('returns the created resource', () => {
+      const resource = _.clone(createPayload);
+      resource.id = createResponse.result.id;
+
+      expect(createResponse.result).to.have.property('_metadata');
+      expect(createResponse.result._metadata).to.have.property('created');
+      expect(createResponse.result._metadata.created).to.be.an.instanceOf(Date);
+
+      const result = _.omit(createResponse.result, '_metadata');
+      expect(result).to.deep.equal(resource);
     });
   });
 
