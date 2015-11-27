9a676e4285fff320c46524445b59e9f37421369e
Chris Impey <chris@bigwednesday.io>
2015-11-27 13:08:07 +0000

    setup docker compose to use dev datastore for postcodes, with preloaded data for tests

--- spec/hooks.js
+++ spec/hooks.js
@@ -3,6 +3,17 @@
 const _ = require('lodash');
 const dataset = require('../lib/models/dataset');
 
+const preloadPostcodeData = () => {
+  const entities = [
+    {key: dataset.key(['Postcode', 'EC2Y9AR']), data: {postcode: 'EC2Y9AR', place: 'Islington', district: 'Islington', region: 'London', county: 'Greater London', country: 'England'}},
+    {key: dataset.key(['Postcode', 'SE228LY']), data: {postcode: 'SE228LY', place: '', district: 'Southwark', region: 'London', county: 'Greater London', country: 'England'}}
+  ];
+
+  return new Promise((resolve, reject) => {
+    dataset.save(entities, err => err ? reject(err) : resolve());
+  });
+};
+
 module.exports.deleteTestData = kind => {
   const query = dataset.createQuery(kind);
 
@@ -30,7 +41,11 @@ module.exports.deleteTestData = kind => {
   });
 };
 
+before(() => preloadPostcodeData());
+
 afterEach(() => Promise.all([
   module.exports.deleteTestData('Supplier'),
   module.exports.deleteTestData('Depot')
 ]));
+
+after(() => module.exports.deleteTestData('Postcode'));
