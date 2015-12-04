c6627532f96231383967d57d7bc2dae2dca03fec
Chris Impey <chris@bigwednesday.io>
2015-12-04 10:52:42 +0000

    put route to update linked product

--- spec/suppliers_{id}_linked_products_{id}.js
+++ spec/suppliers_{id}_linked_products_{id}.js
@@ -9,19 +9,20 @@ describe('/suppliers/{id}/linked_products/{id}', () => {
   const linkedProductPayload = {product_id: '1'};
   let createSupplierResponse;
   let createResponse;
-  let getResponse;
+
+  beforeEach(() =>
+    specRequest({url: '/suppliers', method: 'POST', payload: supplierPayload})
+      .then(response => {
+        createSupplierResponse = response;
+        return specRequest({url: `${response.headers.location}/linked_products`, method: 'POST', payload: linkedProductPayload});
+      })
+      .then(response => createResponse = response));
 
   describe('get', () => {
-    before(() =>
-      specRequest({url: '/suppliers', method: 'POST', payload: supplierPayload})
-        .then(response => {
-          createSupplierResponse = response;
-          return specRequest({url: `${response.headers.location}/linked_products`, method: 'POST', payload: linkedProductPayload});
-        })
-        .then(response => {
-          createResponse = response;
-          return specRequest({url: response.headers.location, method: 'GET'});
-        })
+    let getResponse;
+
+    beforeEach(() =>
+      specRequest({url: createResponse.headers.location, method: 'GET'})
         .then(response => getResponse = response));
 
     it('returns http 200', () => {
@@ -50,6 +51,54 @@ describe('/suppliers/{id}/linked_products/{id}', () => {
 
     it('returns http 404 when linked product does not exist', () =>
       specRequest({url: `${createSupplierResponse.headers.location}/linked_products/abc`, method: 'GET'})
+        .then(response => expect(response.statusCode).to.equal(404)));
+  });
+
+  describe('put', () => {
+    const updatePayload = Object.assign({}, linkedProductPayload, {product_id: '2'});
+    let updateResponse;
+    let getUpdatedResponse;
+
+    beforeEach(() =>
+      specRequest({url: createResponse.headers.location, method: 'PUT', payload: updatePayload})
+        .then(response => updateResponse = response)
+        .then(() => specRequest({url: createResponse.headers.location, method: 'GET'}))
+        .then(response => getUpdatedResponse = response));
+
+    it('returns http 200', () => {
+      expect(updateResponse.statusCode).to.equal(200);
+    });
+
+    it('returns the id', () => {
+      expect(updateResponse.result.id).to.equal(createResponse.result.id);
+    });
+
+    it('returns created date', () => {
+      expect(updateResponse.result._metadata.created).to.deep.equal(createResponse.result._metadata.created);
+    });
+
+    it('progresses the updated date', () => {
+      expect(updateResponse.result._metadata.updated).to.be.a('date');
+      expect(updateResponse.result._metadata.updated.toISOString()).to.be.above(createResponse.result._metadata.updated.toISOString());
+    });
+
+    it('returns the updated linked product resource', () => {
+      expect(_.omit(updateResponse.result, '_metadata', 'id')).to.deep.equal(updatePayload);
+    });
+
+    it('persists the update', () => {
+      expect(getUpdatedResponse.result).to.deep.equal(updateResponse.result);
+    });
+
+    it('returns http 404 when supplier does not exist', () =>
+      specRequest({url: '/suppliers/abc/linked_products/1', method: 'PUT', payload: updatePayload})
+        .then(response => {
+          expect(response.statusCode).to.equal(404);
+          expect(response.result).to.have.property('message', 'Supplier "abc" not found.');
+        }));
+
+    it('returns http 404 when linked product does not exist', () =>
+      specRequest({url: `${createSupplierResponse.headers.location}/linked_products/abc`, method: 'PUT', payload: updatePayload})
         .then(response => expect(response.statusCode).to.equal(404)));
   });
 });
